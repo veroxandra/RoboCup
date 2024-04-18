320052dc8053d4c77ad6064e5ea205a2f1998e5a
jogaja90 <garciajavegajose@gmail.com>
2024-04-18 14:16:07 +0200

    Primera semana

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,9 +1,44 @@
-gnome-terminal -- ./player 2222 &
+gnome-terminal -- bash -c "./player 2222 pOESIAC" &
 sleep 1
-gnome-terminal -- ./player 2223 & 
+gnome-terminal -- bash -c "./player 2223 pOESIAC" &
 sleep 1
-gnome-terminal -- ./player 2224 & 
+gnome-terminal -- bash -c "./player 2224 pOESIAC" &
 sleep 1
-gnome-terminal -- ./player 2225 &
+gnome-terminal -- bash -c "./player 2225 pOESIAC" &
 sleep 1
-gnome-terminal -- ./player 2226 &
+gnome-terminal -- bash -c "./player 2226 pOESIAC" &
+sleep 1
+gnome-terminal -- bash -c "./player 2227 pOESIAC" &
+sleep 1
+gnome-terminal -- bash -c "./player 2228 pOESIAC" &
+sleep 1
+gnome-terminal -- bash -c "./player 2229 pOESIAC" &
+sleep 1
+gnome-terminal -- bash -c "./player 2230 pOESIAC" &
+sleep 1
+gnome-terminal -- bash -c "./player 2231 pOESIAC" &
+sleep 1
+gnome-terminal -- bash -c "./player 2232 pOESIAC" &
+sleep 1
+gnome-terminal -- bash -c "./player 2233 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2234 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2235 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2236 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2237 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2238 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2239 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2240 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2241 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2242 pOESIACA" &
+sleep 1
+gnome-terminal -- bash -c "./player 2243 pOESIACA" &
+sleep 1
--- main.cpp
+++ main.cpp
@@ -6,9 +6,39 @@ using namespace std;
 
 #include <MinimalSocket/udp/UdpSocket.h>
 
+
+vector<string> vectorpalabras(string const &ejercicio){
+  vector<string> resultado;
+  string aux=ejercicio;
+
+  int pos=aux.find(' ');
+    if(pos!=-1)
+      resultado.push_back(aux.substr(0,pos));
+  for(int i=0;i<ejercicio.size();i++){
+    if(ejercicio.at(i)==' '){
+      aux=ejercicio.substr(i+1,ejercicio.size());
+      pos=aux.find(' ');
+      if(pos!=-1)
+        resultado.push_back(aux.substr(0,pos));
+    }
+  }
+  resultado.push_back(aux);
+  return resultado;
+}
+struct Posicion{
+    int x;
+    int y;
+};
+
+string crearMove(Posicion pos){
+    string movimiento="(move "+to_string(pos.x)+" "+to_string(pos.y)+")";
+    return movimiento;
+}
+
 int main(int argc, char* argv[])
 {
     int num;
+    string argumentoString = argv[2];
     MinimalSocket::Port this_socket_port = atoi(argv[0]);
     cout << "Creating a UDP socket" << endl;
     MinimalSocket::udp::Udp<true> udp_socket(this_socket_port, MinimalSocket::AddressFamily::IP_V6);
@@ -21,10 +51,11 @@ int main(int argc, char* argv[])
     }
         // send a message to another udp
         MinimalSocket::Address other_recipient_udp = MinimalSocket::Address{"127.0.0.1", 6000};
-        udp_socket.sendTo("(init pOESIAC++(version 19))", other_recipient_udp);
+        udp_socket.sendTo("(init "+argumentoString+"(version 19))", other_recipient_udp);
+        //udp_socket.sendTo("(init pOESIAC(version 19))", other_recipient_udp);
         cout << "Message sent" << endl;
         // receive a message from another udp reaching this one
-        std::size_t message_max_size = 1000;
+        std::size_t message_max_size = 10000;
         cout << "Waiting for a message" << endl;
         auto received_message = udp_socket.receive(message_max_size);
         // check the sender address
@@ -34,61 +65,52 @@ int main(int argc, char* argv[])
         // actually received
         std::string received_message_content = received_message->received_message;
         cout << received_message_content << endl;
-        int jugador=int(received_message_content.at(8))-int('0');
-        cout << "El jugador es: "<<jugador <<endl;
+        string jugador=vectorpalabras(received_message_content).at(2);
+        vector <string>palabras=vectorpalabras(received_message_content);
+        for(const auto &palabra : palabras) {
+              cout << palabra << endl;
+          }
         MinimalSocket::Address server_upd = MinimalSocket::Address{"127.0.0.1", other_sender_udp.getPort()};
-        switch(jugador){
-            case 1:{
-            udp_socket.sendTo("(move -50 0)", server_upd);
-            cout << "Pa la posi 0 0" << endl;
-                break;
-            }
-            case 2:{
-            udp_socket.sendTo("(move -35 -20)", server_upd);
-            cout << "Pa la posi 50 50" << endl;
-                break;
-            }
-            case 3:{
-            udp_socket.sendTo("(move -35 0)", server_upd);
-            cout << "Pa la posi 25 25" << endl;
-
-                break;
-            }
-            case 4:{
-            udp_socket.sendTo("(move -35 20)", server_upd);
-            cout << "Pa la posi 10 10" << endl;
-
-                break;
-            }
-            case 5:{
-            udp_socket.sendTo("(move -20 -25)", server_upd);
-                break;
-            }
-            case 6:{
-            udp_socket.sendTo("(move -18 -9)", server_upd);
-                break;
-            }
-            case 7:{
-            udp_socket.sendTo("(move  -18 5)", server_upd);
-                break;
-            }
-            case 8:{
-            udp_socket.sendTo("(move -20 20)", server_upd);
-                break;
-            }
-            case 9:{
-            udp_socket.sendTo("(move -2 -18)", server_upd);
-                break;
-            }
-            case 10:{
-            udp_socket.sendTo("(move 2 2)", server_upd);
-                break;
-            }
-            case 11:{
-            udp_socket.sendTo("(move 2 2)", server_upd);
-                break;
-            }
+        vector<Posicion> posiciones={{50,0},{35,-20},{35,20},{20,-25},{18,-9},{18,5},{20,20},{2,-18},{28,-18},{35,11},{5,0}};
+        //if(vectorpalabras(received_message_content).at(1)=="l"){
+            for(auto &p:posiciones){
+                p.x=-p.x;
+            //}
         }
+        if (jugador == "1") {
+              udp_socket.sendTo(crearMove(posiciones.at(0)), server_upd);
+              cout << crearMove(posiciones.at(0)) << endl;
+          } else if (jugador == "2") {
+            udp_socket.sendTo(crearMove(posiciones.at(1)), server_upd);
+            cout << crearMove(posiciones.at(1)) << endl;
+          } else if (jugador == "3") {
+            udp_socket.sendTo(crearMove(posiciones.at(2)), server_upd);
+            cout << crearMove(posiciones.at(2)) << endl;
+          } else if (jugador == "4") {
+            udp_socket.sendTo(crearMove(posiciones.at(3)), server_upd);
+            cout << crearMove(posiciones.at(3)) << endl;
+          } else if (jugador == "5") {
+            udp_socket.sendTo(crearMove(posiciones.at(4)), server_upd);
+            cout << crearMove(posiciones.at(4)) << endl;
+          } else if (jugador == "6") {
+            udp_socket.sendTo(crearMove(posiciones.at(5)), server_upd);
+            cout << crearMove(posiciones.at(5)) << endl;
+          } else if (jugador == "7") {
+            udp_socket.sendTo(crearMove(posiciones.at(6)), server_upd);
+            cout << crearMove(posiciones.at(6)) << endl;
+          } else if (jugador == "8") {
+            udp_socket.sendTo(crearMove(posiciones.at(7)), server_upd);
+            cout << crearMove(posiciones.at(7)) << endl;
+          } else if (jugador == "9") {
+            udp_socket.sendTo(crearMove(posiciones.at(8)), server_upd);
+            cout << crearMove(posiciones.at(8)) << endl;
+          } else if (jugador == "10") {
+            udp_socket.sendTo(crearMove(posiciones.at(9)), server_upd);
+            cout << crearMove(posiciones.at(9)) << endl;
+          } else if (jugador == "11") {
+            udp_socket.sendTo(crearMove(posiciones.at(10)), server_upd);
+            cout << crearMove(posiciones.at(10)) << endl;
+          }
         int i=0;
         while (true)
         {
