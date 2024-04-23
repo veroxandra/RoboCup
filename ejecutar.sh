3146c2f6f992a613573391fda0800b801d833f91
Verónica Matos <148332830+veroxandra@users.noreply.github.com>
2024-04-23 13:34:09 +0200

    Update ejecutar.sh

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,44 +1,24 @@
-gnome-terminal -- bash -c "./player 2222 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2223 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2224 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2225 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2226 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2227 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2228 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2229 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2230 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2231 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2232 pOESIAC" &
-sleep 1
-gnome-terminal -- bash -c "./player 2233 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2234 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2235 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2236 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2237 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2238 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2239 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2240 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2241 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2242 pOESIACA" &
-sleep 1
-gnome-terminal -- bash -c "./player 2243 pOESIACA" &
-sleep 1
+#!/bin/bash
+
+gnome-terminal --tab --title="Programa 2222" -- bash -c "./player 2222 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2223" -- bash -c "./player 2223 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2224" -- bash -c "./player 2224 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2225" -- bash -c "./player 2225 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2226" -- bash -c "./player 2226 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2227" -- bash -c "./player 2227 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2228" -- bash -c "./player 2228 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2229" -- bash -c "./player 2229 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2230" -- bash -c "./player 2230 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2231" -- bash -c "./player 2231 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2232" -- bash -c "./player 2232 pOESIAC; exec bash" &
+gnome-terminal --tab --title="Programa 2233" -- bash -c "./player 2233 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2234" -- bash -c "./player 2234 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2235" -- bash -c "./player 2235 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2236" -- bash -c "./player 2236 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2237" -- bash -c "./player 2237 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2238" -- bash -c "./player 2238 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2239" -- bash -c "./player 2239 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2240" -- bash -c "./player 2240 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2241" -- bash -c "./player 2241 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2242" -- bash -c "./player 2242 pOESIACA; exec bash" &
+gnome-terminal --tab --title="Programa 2243" -- bash -c "./player 2243 pOESIACA; exec bash" &
