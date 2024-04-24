09ab60bf831ac5b6537e5a65492934047bec216f
jogaja90 <garciajavegajose@gmail.com>
2024-04-24 12:34:12 +0200

    Cosas nuevas

--- funciones.h
+++ funciones.h
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <thread> // Para std::this_thread::sleep_for
 #include <chrono> // Para std::chrono::seconds
+#include <MinimalSocket/udp/UdpSocket.h>
+
 using namespace std;
 
 inline vector<string> vectorpalabras(string const &ejercicio){
@@ -90,4 +92,70 @@ vector<string> encontrarStringConPrefijo(const string& str, const string& prefij
     return vectorpalabras(""); // Retorna una cadena vacía si no se encuentra el prefijo
 }
 
+void PosicionarJugador(string &jugador, MinimalSocket::Address server_udp,MinimalSocket::udp::Udp<true>& udp_socket,string argumentoString) {
+vector<Posicion> posiciones={{50,0},{35,-20},{35,20},{20,-25},{18,-9},{18,5},{20,20},{2,-18},{28,-18},{35,11},{5,0}};
+//if(vectorpalabras(received_message_content).at(1)=="l"){
+    for(auto &p:posiciones){
+        p.x=-p.x;
+    //}
+}
+int player= stoi(jugador);
+        switch(player){
+            case 1:
+                udp_socket.sendTo(crearMove(posiciones.at(0)), server_udp);
+                cout << crearMove(posiciones.at(0)) << endl;
+                break;
+            case 2:
+                udp_socket.sendTo(crearMove(posiciones.at(1)), server_udp);
+                cout << crearMove(posiciones.at(1)) << endl;
+
+                break;
+            case 3:
+                udp_socket.sendTo(crearMove(posiciones.at(2)), server_udp);
+                cout << crearMove(posiciones.at(2)) << endl;
+
+                break;
+            case 4:
+                udp_socket.sendTo(crearMove(posiciones.at(3)), server_udp);
+                cout << crearMove(posiciones.at(3)) << endl;
+
+                break;
+            case 5:
+                udp_socket.sendTo(crearMove(posiciones.at(4)), server_udp);
+                cout << crearMove(posiciones.at(4)) << endl;
+
+                break;
+            case 6:
+                udp_socket.sendTo(crearMove(posiciones.at(5)), server_udp);
+                cout << crearMove(posiciones.at(5)) << endl;
+                break;
+            case 7:
+                udp_socket.sendTo(crearMove(posiciones.at(6)), server_udp);
+                cout << crearMove(posiciones.at(6)) << endl;
+
+                break;
+            case 8:
+                udp_socket.sendTo(crearMove(posiciones.at(7)), server_udp);
+                cout << crearMove(posiciones.at(7)) << endl;
+
+                break;
+            case 9:
+                udp_socket.sendTo(crearMove(posiciones.at(8)), server_udp);
+                cout << crearMove(posiciones.at(8)) << endl;
+                break;
+            case 10:
+                udp_socket.sendTo(crearMove(posiciones.at(9)), server_udp);
+                cout << crearMove(posiciones.at(9)) << endl;
+                break;
+            case 11:
+                udp_socket.sendTo(crearMove(posiciones.at(10)), server_udp);
+                cout << crearMove(posiciones.at(10)) << endl;
+                    break;
+                }
+        if(argumentoString=="pOESIACA"){
+            std::this_thread::sleep_for(std::chrono::seconds(1)); // Espera 1 segundo
+            udp_socket.sendTo("(turn 180)", server_udp);
+        }
+
+}
 #endif // FUNCIONES_H
