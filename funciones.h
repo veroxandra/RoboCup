5d6ea9c4d3f979f4ecf52818c167b92b0c6a27c4
marcosvmrm <vallem.marcos@gmail.com>
2024-04-24 13:58:53 +0200

    Cambios chulos

--- funciones.h
+++ funciones.h
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <thread> // Para std::this_thread::sleep_for
 #include <chrono> // Para std::chrono::seconds
+#include <MinimalSocket/udp/UdpSocket.h>
 using namespace std;
 
 inline vector<string> vectorpalabras(string const &ejercicio){
@@ -89,5 +90,119 @@ vector<string> encontrarStringConPrefijo(const string& str, const string& prefij
     }
     return vectorpalabras(""); // Retorna una cadena vacía si no se encuentra el prefijo
 }
+void PosicionarJugador(string &jugador, MinimalSocket::Address server_udp,MinimalSocket::udp::Udp<true>& udp_socket,string argumentoString) {
+    vector<Posicion> posiciones={{50,0},{35,-20},{35,20},{20,-25},{18,-9},{18,5},{20,20},{2,-18},{28,-18},{35,11},{5,0}};
+    //if(vectorpalabras(received_message_content).at(1)=="l"){
+    for(auto &p:posiciones){
+        p.x=-p.x;
+        //}
+    }
+    int player= stoi(jugador);
 
+    switch(player){
+    case 1:
+        udp_socket.sendTo(crearMove(posiciones.at(0)), server_udp);
+        cout << crearMove(posiciones.at(0)) << endl;
+        break;
+    case 2:
+        udp_socket.sendTo(crearMove(posiciones.at(1)), server_udp);
+        cout << crearMove(posiciones.at(1)) << endl;
+
+        break;
+    case 3:
+        udp_socket.sendTo(crearMove(posiciones.at(2)), server_udp);
+        cout << crearMove(posiciones.at(2)) << endl;
+
+        break;
+    case 4:
+        udp_socket.sendTo(crearMove(posiciones.at(3)), server_udp);
+        cout << crearMove(posiciones.at(3)) << endl;
+
+        break;
+    case 5:
+        udp_socket.sendTo(crearMove(posiciones.at(4)), server_udp);
+        cout << crearMove(posiciones.at(4)) << endl;
+
+        break;
+    case 6:
+        udp_socket.sendTo(crearMove(posiciones.at(5)), server_udp);
+        cout << crearMove(posiciones.at(5)) << endl;
+        break;
+    case 7:
+        udp_socket.sendTo(crearMove(posiciones.at(6)), server_udp);
+        cout << crearMove(posiciones.at(6)) << endl;
+
+        break;
+    case 8:
+        udp_socket.sendTo(crearMove(posiciones.at(7)), server_udp);
+        cout << crearMove(posiciones.at(7)) << endl;
+
+        break;
+    case 9:
+        udp_socket.sendTo(crearMove(posiciones.at(8)), server_udp);
+        cout << crearMove(posiciones.at(8)) << endl;
+        break;
+    case 10:
+        udp_socket.sendTo(crearMove(posiciones.at(9)), server_udp);
+        cout << crearMove(posiciones.at(9)) << endl;
+        break;
+    case 11:
+        udp_socket.sendTo(crearMove(posiciones.at(10)), server_udp);
+        cout << crearMove(posiciones.at(10)) << endl;
+        break;
+    }
+    if(argumentoString=="pOESIACA"){
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // Espera 1 segundo
+        udp_socket.sendTo("(turn 180)", server_udp);
+    }
+
+}
+
+struct Lectura{
+    string porteria_der;
+    string porteria_izq;
+    string  pelota;
+};
+template<typename T>
+Lectura ClasificaDatos (string &tipo, vector<string>  &cadenas) {
+    vector<string> valor,vectoria,valor2,valor3;
+    Lectura lectura;
+    if(tipo=="see"){
+        cout<<"Encontrado el see"<<endl;
+        for(auto parentesis:cadenas){
+            cout<<parentesis<<endl;
+            valor=encontrarStringConPrefijo(parentesis,"(b)");//Buscar en todos los parentesis el de (b)
+            valor2=encontrarStringConPrefijo(parentesis,"(g r)");//Buscar en todos los parentesis el de (g r)
+            valor3=encontrarStringConPrefijo(parentesis,"(g l)");//Buscar en todos los parentesis el de (g l)
+
+            if(valor2.size()>1){
+
+                lectura.porteria_der=valor2.at(2);
+
+            }
+            if(valor3.size()>1){
+
+                lectura.porteria_izq=valor3.at(2);
+
+            }
+            if(valor.size()>1){
+
+                lectura.pelota=valor.at(1);
+                vectoria=valor;//Guardar vector con los string del (b)
+                cout <<vectoria[1]<<endl;
+                double variable=stod(vectoria[1]);
+                cout <<"La variable transformada es:"<<variable<<endl;
+
+            }
+        }
+        return lectura;
+        for(auto palabras:vectoria){
+            cout<<"Palabra: "<<palabras<<endl;
+        }
+
+    }else{
+        cout << "El tipo es: "<<tipo<<endl;
+    }
+
+}
 #endif // FUNCIONES_H
--- main.cpp
+++ main.cpp
@@ -46,70 +46,8 @@ int main(int argc, char* argv[])
               cout << palabra << endl;
           }
         MinimalSocket::Address server_udp = MinimalSocket::Address{"127.0.0.1", other_sender_udp.getPort()};
-        vector<Posicion> posiciones={{50,0},{35,-20},{35,20},{20,-25},{18,-9},{18,5},{20,20},{2,-18},{28,-18},{35,11},{5,0}};
-        //if(vectorpalabras(received_message_content).at(1)=="l"){
-            for(auto &p:posiciones){
-                p.x=-p.x;
-            //}
-        }
-        int player= stoi(jugador);
+        PosicionarJugador(jugador, server_udp,udp_socket,argumentoString);
 
-                switch(player){
-                    case 1:
-                        udp_socket.sendTo(crearMove(posiciones.at(0)), server_udp);
-                        cout << crearMove(posiciones.at(0)) << endl;
-                        break;
-                    case 2:
-                        udp_socket.sendTo(crearMove(posiciones.at(1)), server_udp);
-                        cout << crearMove(posiciones.at(1)) << endl;
-
-                        break;
-                    case 3:
-                        udp_socket.sendTo(crearMove(posiciones.at(2)), server_udp);
-                        cout << crearMove(posiciones.at(2)) << endl;
-
-                        break;
-                    case 4:
-                        udp_socket.sendTo(crearMove(posiciones.at(3)), server_udp);
-                        cout << crearMove(posiciones.at(3)) << endl;
-
-                        break;
-                    case 5:
-                        udp_socket.sendTo(crearMove(posiciones.at(4)), server_udp);
-                        cout << crearMove(posiciones.at(4)) << endl;
-
-                        break;
-                    case 6:
-                        udp_socket.sendTo(crearMove(posiciones.at(5)), server_udp);
-                        cout << crearMove(posiciones.at(5)) << endl;
-                        break;
-                    case 7:
-                        udp_socket.sendTo(crearMove(posiciones.at(6)), server_udp);
-                        cout << crearMove(posiciones.at(6)) << endl;
-
-                        break;
-                    case 8:
-                        udp_socket.sendTo(crearMove(posiciones.at(7)), server_udp);
-                        cout << crearMove(posiciones.at(7)) << endl;
-
-                        break;
-                    case 9:
-                        udp_socket.sendTo(crearMove(posiciones.at(8)), server_udp);
-                        cout << crearMove(posiciones.at(8)) << endl;
-                        break;
-                    case 10:
-                        udp_socket.sendTo(crearMove(posiciones.at(9)), server_udp);
-                        cout << crearMove(posiciones.at(9)) << endl;
-                        break;
-                    case 11:
-                        udp_socket.sendTo(crearMove(posiciones.at(10)), server_udp);
-                        cout << crearMove(posiciones.at(10)) << endl;
-                            break;
-                        }
-                if(argumentoString=="pOESIACA"){
-                    std::this_thread::sleep_for(std::chrono::seconds(1)); // Espera 1 segundo
-                    udp_socket.sendTo("(turn 180)", server_udp);
-                }
         while (true)
         {
             auto received_message = udp_socket.receive(message_max_size);
@@ -119,6 +57,9 @@ int main(int argc, char* argv[])
             vector<string> cadenas = dividir_en_palabras_parentesis(contenido);
             //cadenas.push_back(tipo);
             cadenas.insert(cadenas.begin(), tipo); // Inserta la primera palabra al principio del vector
+
+
+            auto Estructura = ClasificaDatos<string>(tipo, cadenas);
             vector<string> valor,vectoria,valor2,porteria;
             if(tipo=="see"){
                 cout<<"Encontrado el see"<<endl;
